13ec8cdc66631fe78019a6046a9c85cfc04975f4
D-ixon <dicksonsci@gmail.com>
2026-07-11 22:30:40 +0000

    feat: implement plotly

--- python-programming/normal-distribution.py
+++ python-programming/normal-distribution.py
@@ -5,3 +5,7 @@ X = np.linspace(-5, 5, num=100)
 
 Y = X ** 2
 
+fig = go.Figure()
+fig.add_trace(go.Scatter(x=X, y=Y, mode='lines', name='y = x^2'))
+fig.update_layout(title='Normal Distribution', xaxis_title='X', yaxis_title='Y', showlegend=True)
+fig.show()
