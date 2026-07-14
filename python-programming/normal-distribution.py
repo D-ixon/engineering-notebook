ce42364221f4888e9a489da3e208f03f347b7265
D-ixon <dicksonsci@gmail.com>
2026-07-14 10:58:34 +0000

    fix: resolve looping issues

--- python-programming/normal-distribution.py
+++ python-programming/normal-distribution.py
@@ -6,28 +6,30 @@ import numpy as np
 
 X = np.linspace(-5, 5, num=100)
 
-mu = 1
+mus = [-3, 0, 2]
 two_pi = np.pi * 2
 sigmas = [0.5, 1, 2]
 
-distance_squared = np.square(X - mu)
 
 gauss = go.Figure()
 
 for sigma in sigmas:
-    
-    sigma_square = sigma ** 2   
+
+    sigma_square = sigma ** 2 
 
     denominator = 2 * sigma_square
 
-    normalization_constant = 1 / (sigma * np.sqrt(two_pi))
+    for mu in mus:
 
-    gaussian_shape = np.exp(-distance_squared/ denominator)
+        distance_squared = np.square(X - mu)
 
-    probability_density = normalization_constant * gaussian_shape
+        normalization_constant = 1 / (sigma * np.sqrt(two_pi))
 
-    gauss.add_trace(go.Scatter(x=X, y=probability_density, mode='lines', name=(f" 𝜎 = {sigma}"), line=dict(color='red')))
+        gaussian_shape = np.exp(-distance_squared / denominator)
 
+        probability_density = normalization_constant * gaussian_shape
+
+        gauss.add_trace(go.Scatter(x=X, y=probability_density, mode='lines', name=(f" 𝜎 = {sigma}"), line=dict(color='red')))
 
 
 gauss.update_layout(title = "Gaussian (Normal) Distribution", xaxis_title = "X values", yaxis_title = "the kernel")
