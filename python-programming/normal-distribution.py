53a68edf2c9fbdb0129e6fc7ec0f57ec1d9fb37e
D-ixon <dicksonsci@gmail.com>
2026-07-17 09:45:39 +0000

    refactor: move Gaussian PDF calculation into function

--- python-programming/normal-distribution.py
+++ python-programming/normal-distribution.py
@@ -10,26 +10,25 @@ mus = [-3, 0, 2]
 two_pi = np.pi * 2
 sigmas = [0.5, 1, 2]
 
+def gaussian_pdf(x, mu, sigma):
+    sigma_square = sigma ** 2
+    denominator = 2 * sigma_square
+    distance_squared = np.square(x - mu)
+    gaussian_shape = np.exp(-distance_squared / denominator)
+    normalization_constant = 1 / (sigma * np.sqrt(two_pi))
+    probability_density = normalization_constant * gaussian_shape
+
+    return probability_density
 
 gauss = go.Figure()
 
 for sigma in sigmas:
-
-    sigma_square = sigma ** 2 
-
-    denominator = 2 * sigma_square
-
+    
     for mu in mus:
 
-        distance_squared = np.square(X - mu)
+        probability_density = gaussian_pdf(X, mu, sigma)
 
-        normalization_constant = 1 / (sigma * np.sqrt(two_pi))
-
-        gaussian_shape = np.exp(-distance_squared / denominator)
-
-        probability_density = normalization_constant * gaussian_shape
-
-        gauss.add_trace(go.Scatter(x=X, y=probability_density, mode='lines', name=(f" 𝜎 = {sigma}"), line=dict(color='red')))
+        gauss.add_trace(go.Scatter(x=X, y=probability_density, mode='lines', name=(f" 𝜎, μ = {sigma, mu}")))
 
 
 gauss.update_layout(title = "Gaussian (Normal) Distribution", xaxis_title = "X values", yaxis_title = "the kernel")
