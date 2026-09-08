117ddb08c52e1c11310af3b9972260a5a7d81803
Kofi <dicksonsci@gmail.com>
2026-09-08 21:26:09 +0000

    feat: java syntax

--- java/HelloWorld.java
+++ java/HelloWorld.java
@@ -1,5 +1,14 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        int a = 5;
+        int b = 10;
+        int sum = a + b;
+        
+        if (sum > 10) {
+            System.out.println("The sum is greater than 10.");
+        } else {
+            System.out.println("The sum is 10 or less.");
+        }
     }
 }
