f789cbe68623c9c9cfa8aef71cf43200eee93c4f
Kofi <dicksonsci@gmail.com>
2026-09-23 11:48:32 +0000

    remove method

---
+++ java/subs.java
@@ -0,0 +1,20 @@
+public class subs {
+    private String name;
+
+    // Constructor that accepts a String argument
+    public subs (String name) {
+        this.name = name;
+    }
+
+    // Getter method (optional, but recommended to access the name)
+    public String getName() {
+        return name;
+    }
+
+    // Setter method (optional, to change the name later)
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
+}
